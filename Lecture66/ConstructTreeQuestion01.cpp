958922e607c6cbd7e43df01497b6f6bb04572ca0
shubhampatel27 <shubhampatel0305@gmail.com>
2023-03-03 16:55:39 +0530

    Lecture 66

--- Lecture66/ConstructTreeQuestion01.cpp
+++ Lecture66/ConstructTreeQuestion01.cpp
@@ -1,21 +1,50 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <map>
+//{ Driver Code Starts
+//
+
+#include<bits/stdc++.h>
 using namespace std;
-// your code here
+
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    data = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+
+void printPostOrder(Node *root)
+{
+	if(root==NULL)
+		return;
+	printPostOrder(root->left);
+	printPostOrder(root->right);
+	cout<<root->data<<" ";
+}
+
+// } Driver Code Ends
+
+/*Complete the code here.
+Node is as follows:
+struct Node
+{
+  int data;
+  Node* left;
+  Node* right;
+};
+*/
+
+
+
 class Solution
 {
 public:
-    void createMapping(int in[], map<int, int> &nodeToIndex, int n)
-    {
-        for (int i = 0; i < n; i++)
-        {
-            nodeToIndex[in[i]] = i;
-             
-        }
-    }
-    Node *solve(int in[], int pre[], int &index, int inorderStart, int inorderEnd, int n, map<int, int> &nodeToIndex)
+    Node *solve(int in[], int pre[], int &index, int inorderStart, int inorderEnd, int n,  map<int, queue<int>> &nodeToIndex)
     {
         // base case
         if (index >= n || inorderStart > inorderEnd)
@@ -24,23 +53,60 @@ public:
         }
         // created a root node for element
         int element = pre[index++];
-        Node *root = new Node(element);
+        Node* root = new Node(element);
 
         // find elements's index in
-        int position = nodeToIndex[element];
+        int position = nodeToIndex[element].front();
+        nodeToIndex[element].pop();
         // recursive calls
         root->left = solve(in, pre, index, inorderStart, position - 1, n, nodeToIndex);
-        root->right = solve(in, pre, index, inorderStart, position - 1, n, nodeToIndex);
+        root->right = solve(in, pre, index, position +1, inorderEnd, n, nodeToIndex);
 
         return root;
     }
     Node *buildTree(int in[], int pre[], int n)
     {
         int preOrderIndex = 0;
-        map<int, int> nodeToIndex;
+        map<int, queue<int>> nodeToIndex;
         //  create nodes to index mapping
-        createMapping(in, nodeToIndex, n);
+        for (int i = 0; i < n; i++)
+        {
+            nodeToIndex[in[i]].push(i);
+        }
         Node *ans = solve(in, pre, preOrderIndex, 0, n - 1, n, nodeToIndex);
         return ans;
     }
 };
+// 7
+// 7 3 11 1 17 3 18 inorder 
+// 1 3 7 11 3 17 18 preorder
+
+
+
+
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		
+		int inorder[n], preorder[n];
+		for(int i=0; i<n; i++)
+			cin>> inorder[i];
+		for(int i=0; i<n; i++)
+			cin>> preorder[i];
+		Solution obj;
+		Node *root = obj.buildTree(inorder, preorder, n);
+		printPostOrder(root);
+		cout<< endl;
+	}
+}
+
+// } Driver Code Ends
