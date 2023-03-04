2c0431dd29b71ffc51edd8e38768e3ce5b3b2fb4
shubhampatel27 <shubhampatel0305@gmail.com>
2023-03-04 12:39:13 +0530

    Lecture69 BST

--- Lecture69/SearchInBST.cpp
+++ Lecture69/SearchInBST.cpp
@@ -29,3 +29,41 @@ bool searchInBST(BinaryTreeNode<int> *root, int x)
     else
         return searchInBST(root->right, x);
 }
+
+
+//  iterative way 
+
+
+#include <bits/stdc++.h> 
+/*
+    Following is the Binary Tree node structure:
+
+    class BinaryTreeNode {
+    public : 
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+*/
+
+bool searchInBST(BinaryTreeNode<int> *root, int x) {
+    // Write your code here.
+    BinaryTreeNode<int> * temp = root;
+    while(temp != NULL){
+        if(temp->data == x) return true;
+
+        else if (temp->data > x) {
+            temp = temp->left;
+        } 
+        else {
+            temp = temp->right;
+        }
+    }
+    return false;
+}
