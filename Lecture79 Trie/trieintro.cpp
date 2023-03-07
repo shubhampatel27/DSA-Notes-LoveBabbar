ffc7ca63752e239a6de479709c91d1e7b067cb46
shubhampatel27 <shubhampatel0305@gmail.com>
2023-03-07 18:59:53 +0530

    Lecture 79

---
+++ Lecture79 Trie/trieintro.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+using namespace std;
+
+class TrieNode
+{
+public:
+    char data;
+    TrieNode *children[26];
+    bool isTerminal;
+
+    TrieNode(char ch)
+    {
+        data = ch;
+        for (int i = 0; i < 26; i++)
+        {
+            children[i] = NULL;
+        }
+        isTerminal = false;
+    }
+};
+class Trie
+{
+public:
+    TrieNode *root;
+    Trie()
+    {
+        root = new TrieNode('\0');
+    }
+    void insertUtil(TrieNode *root, string word)
+    {
+        // base case
+        if (word.length() == 0)
+        {
+            root->isTerminal = true;
+            return;
+        }
+        // assumption, word will be in CAPS
+        int index = word[0] - 'A';
+        TrieNode *child;
+
+        // present
+        if (root->children[index] != NULL)
+        {
+            child = root->children[index];
+        }
+        else
+        {
+            child = new TrieNode(word[0]);
+            root->children[index] = child;
+        }
+        // RECURSION
+        insertUtil(child, word.substr(1));
+    }
+    void insertWord(string word)
+    {
+        insertUtil(root, word);
+    }
+
+    bool searchUtil(TrieNode *root, string word)
+    {
+        if(word.length() == 0)
+        {
+            return root->isTerminal;
+        }
+        // assumption, word will be in CAPS
+        int index = word[0] - 'A';
+        TrieNode *child;
+
+        // present
+        if (root->children[index] != NULL)
+        {
+            child = root->children[index];
+        }
+        else return false;
+
+        // recursion
+        return searchUtil(child, word.substr(1));
+    }
+
+    bool searchWord(string word)
+    {
+        return searchUtil(root, word);
+    }
+};
+int main()
+{
+    Trie *t = new Trie();
+    t->insertWord("ARM");
+    t->insertWord("DO");
+    t->insertWord("TIME");
+    cout<< "Present or Not"<< endl << t->searchWord("TIM") << endl << endl;
+    return 0;
+}
+
+// O(l) l= length of word for insertion , search & deletion
+//  removal code i will update soon please share and follow for more such Amazing things 
